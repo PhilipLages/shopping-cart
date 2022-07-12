f30e3c481c6ca37090f01ad526e8c77795339e99
Philip Lages <psilvaelages@gmail.com>
2022-07-11 21:55:01 -0300

    Add requirement 7

--- tests/getSavedCartItems.test.js
+++ tests/getSavedCartItems.test.js
@@ -3,7 +3,17 @@ const getSavedCartItems = require('../helpers/getSavedCartItems');
 
 localStorageSimulator('getItem');
 
-describe('4 - Teste a função getSavedCartItems', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+describe('4 - getSavedCartItems function tests', () => {
+  it('should call localStorage.getItem method', () => {
+    getSavedCartItems();
+    const storage = localStorage.getItem;
+    expect(storage).toHaveBeenCalled();
+  })
+  it('should call localStorage.getItem method with cartItem as an argument', () => {
+    getSavedCartItems();
+    const arg = 'cartItems';
+    const storage = localStorage.getItem;
+    expect(storage).toBeCalledWith(arg);
+  })
+  // fail('Teste vazio');
 });
